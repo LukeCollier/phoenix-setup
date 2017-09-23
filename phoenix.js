bb690c54bcece153459fecfc6e55932b2fefa228
Luke Edwin Carlyle Collier <lukecollier@joker.home>
2017-09-23 15:40:15 +0100

    First commit with the initial setup and keybindings for the planned features

---
+++ phoenix.js
@@ -0,0 +1,11 @@
+Phoenix.set({
+	  daemon: true,
+	  openAtLogin: true
+});
+
+const keybindings = {
+	maximise: 'm',
+	screen: ['1','2','3','4','5','6','7','8','9','0'],
+	workspaces: ['1','2','3','4','5','6','7','8','9','0'],
+	segments: ['a','w','s','d']
+}
